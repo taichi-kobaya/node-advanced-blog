4db0557035f239f350c09aff2672db3e4deea960
taichi-kobaya <higoronookonaigadaiji@gmail.com>
2023-02-09 15:48:22 +0900

    MVC化まで完了

---
+++ controllers/blog/updatePost.js
@@ -0,0 +1,12 @@
+const BlogModel = require("../../models/blog")
+
+module.exports = (req, res) => {
+  BlogModel.updateOne({_id: req.params.id}, req.body).exec((error) =>
+  {
+    if(error) {
+      res.render("error", {message: "/blog/updateのエラー"})
+    }else{
+      res.redirect("/")
+    }
+  })
+}
